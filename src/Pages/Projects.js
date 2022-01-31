609313bab18d2d06b057656d3567fe74a66672c8
JK1320 <kaurjus1@gmail.com>
2022-01-31 03:13:59 +0000

    added div for project info + css

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -11,16 +11,21 @@ function Projects() {
       <Header />
       <div className="projects">
         {projectsData.map((project, index) => (
-          <div className="project-container">
-            <h1>{project.project}</h1>
-            <img src={project.image} alt="project information" />
-            <div className="live-link">
-              <a href={project.link} target="_blank" rel="noReferrer">
-                Live Link
-              </a>
-              <a href={project.github} target="_blank" rel="noReferrer">
-                github
-              </a>
+          <div className="projects-main">
+            <div className="project-container">
+              <h1>{project.project}</h1>
+              <img src={project.image} alt="project information" />
+              <div className="live-link">
+                <a href={project.link} target="_blank" rel="noReferrer">
+                  Live Link
+                </a>
+                <a href={project.github} target="_blank" rel="noReferrer">
+                  github
+                </a>
+              </div>
+            </div>
+            <div className="project-info">
+              <p>{project.info}</p>
             </div>
           </div>
         ))}
