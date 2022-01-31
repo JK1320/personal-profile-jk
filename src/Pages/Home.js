7b9199e78024de183d89af6fcd4fb03a94b2f502
JK1320 <kaurjus1@gmail.com>
2022-01-31 02:29:23 +0000

    spelling correction

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
             I joined Code Your Future in 2020 and took a great opportunity to
             become a Full Stack Web Developer. I learned HTML/CSS,
             JavaScript(React), Node.js and PostgreSQL. Even though My skills are
-            basics at the moment but I practice these skills every day to
+            basic at the moment but I practice these skills every day to
             improve.
           </p>
         </div>
