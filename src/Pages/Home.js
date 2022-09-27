8e11c9895069229502ddd1f3fd386056a1362c78
JK1320 <kaurjus1@gmail.com>
2022-09-27 13:48:04 +0100

    moved code to Navbar component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,30 +1,17 @@
 import ScrollToTop from "../components/ScrollToTop";
-import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 import React from 'react'
 import Footer from "../components/Footer";
-import Projects from "./Projects";
-import Skills from "./Skills";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
+import Quotes from "../components/Quotes";
 
 const Home = () => {
     return (
-      <div className="main">
-        <Header />
-        <div className="home-intro">
-          <div className="heading">
-            <h1>Jasvir Kaur</h1>
-            <h4>Full Stack Web Developer</h4>
-          </div>
-        </div>
-        <div className="about">
-          <h3>About</h3>
-          <p>
-            I joined CodeYourFuture in 2020 and took a great opportunity to
-            become a Full Stack Web Developer. I learned HTML/CSS,
-            JavaScript(React), Node.js and PostgreSQL. Even though My skills are
-            basic at the moment but I practice these skills every day to
-            improve.
-          </p>
-        </div>
+      <div>
+        <Quotes />
+        <Navbar />
+       
         <Skills />
         <Projects />
         <Footer />
