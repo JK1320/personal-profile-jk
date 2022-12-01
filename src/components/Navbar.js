c18d08166b4fefb1786f81371ac6e4afb6b88519
JK1320 <kaurjus1@gmail.com>
2022-12-01 23:47:10 +0000

    rearrange HTML tags & text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,28 +9,25 @@ function Navbar() {
     return (
       <div className="container-navbar">
         <div class="info-nav">
-          <div className="home-intro">
-            <div className="heading">
-              <h1>Jasvir Kaur</h1>
-              <h4>Full Stack Web Developer</h4>
-            </div>
+          <div className="heading">
+            <h1>Jasvir Kaur</h1>
+            <h4>Junior Full Stack Web Developer</h4>
           </div>
           <div className="about">
             <p>
               I joined CodeYourFuture in 2020 and took a great opportunity to
               become a Full Stack Web Developer. I learned HTML/CSS,
-              JavaScript(React), Node.js and PostgreSQL. Even though My skills
-              are basic at the moment but I practice these skills every day to
-              improve.
+              JavaScript(React), Node.js and PostgreSQL. Since graduating I have
+              created few projects for learning on Styled Components,
+              Material-UI and MongoDB. I have recently joined CodeYourFuture
+              Product Dev Team.
             </p>
           </div>
         </div>
         <div className="btn-nav">
-        
-            <Link className="link" to="/resume">
-              Resume
-            </Link>
-         
+          <Link className="link" to="/resume">
+            CV
+          </Link>
         </div>
       </div>
     );
