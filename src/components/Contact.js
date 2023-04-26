4ba295132a894de1b51cf893d1a0918b7bceb9dc
JK1320 <kaurjus1@gmail.com>
2023-04-26 22:53:36 +0100

    added new file for Contact

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import emailjs from "emailjs-com";
+import Footer from './Footer';
+import Navbar from './Navbar';
+
+
+const Contact = () => {
+  return (
+    <div>
+        <Navbar />
+        Contact
+        <Footer />
+    </div>
+  )
+}
+
+export default Contact;
