3e1331028afd250f1b62ac9ff7a474fb7bbe2117
JK1320 <kaurjus1@gmail.com>
2023-04-27 14:08:18 +0100

    added form tag

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React from "react";
 //import emailjs from "emailjs-com";
-import Footer from './Footer';
-import Navbar from './Navbar';
-
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import "../styleSheets/Contact.css";
 
 const Contact = () => {
   return (
-    <div>
-        <Navbar />
-        Contact
-        <Footer />
+    <div className="contact">
+      <Navbar />
+      <form className="contact-form">
+        <input type="text" name="name" id="" placeholder="Name" />
+        <input type="text" name="subject" id="" placeholder="Subject" />
+        <input type="text" name="email" id="" placeholder="Email" />
+        <textarea
+          name="message"
+          id=""
+          cols=""
+          rows="5"
+          placeholder="Message"
+        ></textarea>
+        <button>Submit</button>
+        {/* {done && "Thank you..."} */}
+      </form>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default Contact;
