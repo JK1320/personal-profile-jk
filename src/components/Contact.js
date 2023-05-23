219c7182f439589eab32e984e96874e70960b821
JK1320 <kaurjus1@gmail.com>
2023-05-23 12:56:44 +0100

    used email.js to receive messages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,60 @@
-import React from "react";
-//import emailjs from "emailjs-com";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import "../styleSheets/Contact.css";
 
 const Contact = () => {
+  const form = useRef();
+
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_k819hoc",
+        "template_9ey3a7p",
+        form.current,
+        "TAafFvEsKqP6ItEaL"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+        e.target.reset();
+  };
+
   return (
     <div className="contact">
       <Navbar />
-      <form className="contact-form">
-        <input type="text" name="name" id="" placeholder="Name" />
-        <input type="text" name="subject" id="" placeholder="Subject" />
-        <input type="text" name="email" id="" placeholder="Email" />
+      <form className="contact-form" ref={form} onSubmit={sendEmail}>
+        <input type="text" name="user_name" id="" placeholder="Name" required />
+        <input type="text" name="user_subject" id="" placeholder="Subject" />
+        <input
+          type="text"
+          name="user_email"
+          id=""
+          placeholder="Email"
+          required
+        />
+
         <textarea
           name="message"
           id=""
           cols=""
           rows="5"
           placeholder="Message"
+          required
         ></textarea>
-        <button>Submit</button>
-        {/* {done && "Thank you..."} */}
+        <button type="submit" value="Send" className="btn">
+          Submit
+        </button>
       </form>
       <Footer />
     </div>
